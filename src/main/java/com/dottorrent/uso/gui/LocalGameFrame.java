deb793e3d4c30a8ba4987a42f158d3303af506f2
DotTorrent <pointtorrent@outlook.com>
2020-11-28 05:30:17 +0800

    Create LocalGameFrame.java

---
+++ src/main/java/com/dottorrent/uso/gui/LocalGameFrame.java
@@ -0,0 +1,12 @@
+package com.dottorrent.uso.gui;
+
+import javax.swing.*;
+
+/**
+ * Description here
+ *
+ * @author .torrent
+ * @version 1.0.0 2020/11/28
+ */
+public class LocalGameFrame extends JFrame {
+}
